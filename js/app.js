6035d6d42dd5675baa03ebe1531d1b30d84d16c9
Eduardo <duardohenriqe19@gmail.com>
2023-07-10 19:38:31 -0300

    catalago

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const images = document.querySelectorAll(".cars-text-image");
-
-function activeModal() {
-  this.classList.toggle("active-modal");
-}
-
-images.forEach((image) => {
-  image.addEventListener("click", activeModal);
-})
