ce1810c05f1c688f0a6810882d03467cbcf878d6
Eduardo <duardohenriqe19@gmail.com>
2023-07-12 00:41:19 -0300

    página de detalhes - alterar imagens

--- js/app.js
+++ js/app.js
@@ -27,4 +27,29 @@ function initModal() {
   }
 }
 
+function initChangeImage() {
+  const imageFull = document.querySelector(".js-image-full-content");
+  const imagesScroll = document.querySelectorAll(".js-scroll-images");
+
+  if (imageFull && imagesScroll.length) {
+    // src das imagens
+    function changeImage(event) {
+      const clickedImage = event.target;
+      // transição
+      imageFull.classList.add("hide-image");
+      // atualizar e exibir imagem
+      setTimeout(function() {
+        imageFull.src = clickedImage.src;
+        // esconder imagem
+        imageFull.classList.remove("hide-image");
+      }, 300);
+    }
+
+    imagesScroll.forEach((image) => {
+      image.addEventListener("click", changeImage);
+    });
+  }
+}
+
 initModal();
+initChangeImage();
