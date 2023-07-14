19023670da42b198fa83b2544902d1e07211620e
Eduardo <duardohenriqe19@gmail.com>
2023-07-14 13:12:25 -0300

    clipboard

--- js/app.js
+++ js/app.js
@@ -98,7 +98,34 @@ function initAnimationScroll() {
   window.addEventListener("scroll", animarScroll);
 }
 
+function initClipboard() {
+  const btnShare = document.querySelector("[data-share='share']");
+  const btnCopy = document.querySelector("[data-clipboard='copy']");
+
+  if (btnShare && btnCopy) {
+    function Clipboard() {
+      function copyText() {
+        let textContent = document.querySelector("[data-text='text']");
+        textContent.select();
+        textContent.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+      }
+
+      btnCopy.addEventListener("click", copyText);
+    }
+
+    function showClipboard() {
+      const copyInput = document.querySelector("[data-copy='copy-input']");
+      btnShare.classList.toggle("active");
+      copyInput.classList.toggle("active");
+      Clipboard();
+    }
+    btnShare.addEventListener("click", showClipboard);
+  }
+}
+
 initModal();
 initChangeImage();
 initAccordion();
 initAnimationScroll();
+initClipboard();
